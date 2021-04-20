99c5d6a267987207b2a2d1449bfa5ee18c78a519
adarshraj00 <rajadarshofficial@gmail.com>
2021-04-20 13:07:39 +0530

    g

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-            about page
+            <h1 style={{textAlign: 'center' ,marginTop: "20vh"}}>made by <a href="https://adarshraj.netlify.app" target="_blank">adarsh raj</a></h1>
         </div>
     )
 }
